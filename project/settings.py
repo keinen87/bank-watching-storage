997939351215944069be1d56cda2ba16c25a6e3d
Nurbek Kuspanov <nurbek@inbox.ru>
2021-08-03 15:51:40 +0400

    hided credentials

--- project/settings.py
+++ project/settings.py
@@ -1,25 +1,29 @@
 import os
+from environs import Env
+
+env = Env()
+env.read_env()
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": "checkpoint.devman.org",
-        "PORT": "5434",
-        "NAME": "checkpoint",
-        "USER": "guard",
-        "PASSWORD": "osim5",
+    'default': {
+        'ENGINE': env('ENGINE'),
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ["datacenter"]
 
-SECRET_KEY = "REPLACE_ME"
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
+DEBUG = env.bool("DEBUG", False)
 
 ROOT_URLCONF = "project.urls"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
